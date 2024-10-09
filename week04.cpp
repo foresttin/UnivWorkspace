156e57a8befc0697a00460a104d9b095401eb847
foresttin <169050487+foresttin@users.noreply.github.com>
2024-10-10 00:23:02 +0900

    Update week04.cpp

--- week04.cpp
+++ week04.cpp
@@ -204,3 +204,47 @@ int main() {
 // 실행 결과
 rect1은 정사각형이다.
 rect3는 정사각형이다.
+// 예제 3-7 : Circle 클래스에 소멸자 작성 및 실행
+#include <iostream>
+using namespace std;
+
+class Circle {
+public:
+    int radius;
+
+    Circle();
+    Circle(int r);
+    ~Circle();  // 소멸자
+    double getArea();
+};
+
+Circle::Circle(){
+    radius = 1;
+    cout << "반지름 " << radius << " 원 생성" << endl;
+}
+
+Circle::Circle(int r) {
+    radius = r;
+    cout << "반지름 " << radius << " 원 생성" << endl;
+}
+
+Circle::~Circle() {
+    cout << "반지름 " << radius << " 원 소멸" << endl;
+}
+
+double Circle::getArea() {
+    return 3.14 * radius * radius;
+}
+
+int main() {
+    Circle donut;
+    Circle pizza(30);
+
+    return 0;   // main() 함수가 종료되면 main() 함수의 스택에 생성된 객체가 소멸된다.
+}
+
+// 실행 결과
+반지름 1 원 생성
+반지름 30 원 생성
+반지름 30 원 소멸
+반지름 1 원 소멸
