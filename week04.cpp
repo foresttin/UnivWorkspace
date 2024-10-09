738f282f15a19cbe5b9d9bc8402fe80e973e5d4d
foresttin <169050487+foresttin@users.noreply.github.com>
2024-10-10 00:01:49 +0900

    Update week04.cpp

--- week04.cpp
+++ week04.cpp
@@ -161,3 +161,46 @@ int main() {
 // 실행 결과
 (0,0)
 (10,20)
+
+// 예제 3-6 : Rectangle 클래스 만들기
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+public:
+    int width=1 , height=1;
+    int a, b, c;
+    Rectangle();
+    Rectangle(int a, int b);
+    Rectangle(int c);
+    bool isSquare();
+};
+
+Rectangle::Rectangle() : Rectangle(1) {}    // 위임 생성자
+
+Rectangle::Rectangle(int a, int b) {
+    width = a;
+    height = b;
+}
+Rectangle::Rectangle(int c) {   // 타겟 생성자
+    width = height = c;
+}
+
+bool Rectangle::isSquare() {    // 정사각형이면 true를 리턴하는 멤버 함수
+    if(width == height) return true;
+    else return false;
+}
+
+int main() {
+    Rectangle rect1;
+    Rectangle rect2(3, 5);
+    Rectangle rect3(3);
+    
+    if(rect1.isSquare()) cout << "rect1은 정사각형이다." << endl;
+    if(rect2.isSquare()) cout << "rect2는 정사각형이다." << endl;
+    if(rect3.isSquare()) cout << "rect3는 정사각형이다." << endl;
+}
+
+// 실행 결과
+rect1은 정사각형이다.
+rect3는 정사각형이다.
